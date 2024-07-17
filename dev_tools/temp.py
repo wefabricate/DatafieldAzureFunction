771abfcf635c9400ba9b4a31c8505a20eb4efcf9
Sam <samuel.jarvis@wefabricate.com>
2024-07-17 07:44:21 +0200

    Added dev method to delete datafields

--- dev_tools/temp.py
+++ dev_tools/temp.py
@@ -28,6 +28,32 @@ def create_custom_property(sn, field_name, field_value):
     return True
   else:
     print(f"Failed to create custom property for id: {sn}. Error: {response}")
-    return False  
+    return False
 
-create_custom_property("SN97WMW1X7", "uuid", "")
+def delete_custom_property(sn, field_name):
+  load_dotenv()
+
+  API_ENDPOINT = "https://kosrmwmkgbpwcmqsvffr.supabase.co/rest/v1/rpc/delete_custom_property"
+  api_key = os.getenv("API_KEY")
+  bearer_token = os.getenv("BEARER_TOKEN")
+
+  headers = {
+    "Authorization": f"Bearer {bearer_token}",
+    "Content-Type": "application/json",
+    "apikey": api_key
+  }
+  payload = {
+    "tag_id": sn,
+    "custom_tag_field": field_name
+  }
+
+  response = requests.post(API_ENDPOINT, headers=headers, json=payload)
+
+  if 200 <= response.status_code < 300:
+    print(f"Custom property deleted successfully for id: {sn}")
+    return True
+  else:
+    print(f"Failed to delete custom property for id: {sn}. Error: {response}")
+    return False
+
+# delete_custom_property("SN97WMNMX7", "test")
